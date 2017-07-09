ef343dadc65691e9601f091ff752a73e64e6c905
cleberson <pauluci.cleberson@gmail.com>
2017-07-09 19:13:26 -0300

    introducao ao javascript

---
+++ js/principal.js
@@ -0,0 +1,47 @@
+var titulo = document.getElementById("titulo");
+console.log(titulo);
+titulo.textContent = "Aparecida Nutricionista";
+
+var tabelaPaciente = document.querySelector("#tabela-pacientes");
+var pacientes = tabelaPaciente.getElementsByTagName("tr");
+
+console.log(pacientes);
+
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    calcularImc(paciente);
+}
+
+function calcularImc(paciente) {
+    console.log("O paciente é: " + paciente);
+    console.log("A lista de classes css: " + paciente.classList);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var imc = peso / (altura * altura);
+
+    var isPesoValido = true;
+    var isAlturaValida = true;
+    // validação
+
+    if (peso <= 0 || peso >= 500) {
+        isPesoValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        isAlturaValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.style.backgroundColor = "orange";
+    }
+
+    if (isPesoValido && isAlturaValida) {
+        tdImc.textContent = imc.toPrecision(4);
+    }    
+}
